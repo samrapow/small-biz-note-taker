301084358e9a8d8929e0aea6284cad794750c877
Sam Rapowitz <samrapow@gmail.com>
2022-01-22 19:25:24 -0500

    added html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,15 @@
 const app = rquire('express').Router();
 
-const path = require('path')
+const path = require('path');
+
+// send notes endpoint to the correct file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+// send user to homepage if there's a pathign issue
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports = app;
